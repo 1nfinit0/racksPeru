1e9cd0451cddbb08277e2c6eb0e702ad3847b36b
Tobi <noggnzzz@gmail.com>
2026-03-03 20:53:31 -0500

    1.4v

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://racksperu.vercel.app',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
